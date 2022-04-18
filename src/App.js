5c35afd3508f5739f83c94268bcde7b988e190bf
Redcoder807 <gearonhaynes@gmail.com>
2022-04-18 11:58:02 -0500

    Bonus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // 1d
 import Exercise from "./exercise/Exercise";
 
+// 3f
+import Bonus from "./bonus/Bonus";
+
 function App() {
   return (
     <div>
       {/* 1d */}
       <Exercise />
+      {/* 3g */}
+      <Bonus />
     </div>
   );
 }
